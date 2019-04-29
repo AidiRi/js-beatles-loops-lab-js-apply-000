5d8de74f8d34692799dca15fa160831477c1bf50
Aidiris Rivera <rivera.aidi@gmail.com>
2019-04-29 21:50:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBealelsPlay(){
-  var musicians = [];
-  var instruments = [];
+  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
   var array = [];
   for(var i = 0; i < )
 }
