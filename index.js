7da048bee01934a0a1a7348f796cb26aaefd42a6
Aidiris Rivera <rivera.aidi@gmail.com>
2019-04-29 21:52:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 function theBealelsPlay(){
   const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
   const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-  var array = [];
-  for(var i = 0; i < )
+  var beatlesArr = [];
+  for(var i = 0; i < musicians.length; i ++){
+    beatlesArr.push(musicians[i] + " plays " + instruments[i])
+  }
 }
