91f680c0f30905315a3b0939f956c92babea9003
Aidiris Rivera <rivera.aidi@gmail.com>
2019-04-29 21:53:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // add solution here
-function theBeatelsPlay(){
+function theBeatlesPlay(){
   const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
   const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
   var beatlesArr = [];
