b5b8cc58ee29a4ce0313758effde21b01e0a7977
Aidiris Rivera <rivera.aidi@gmail.com>
2019-04-29 22:01:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ function theBeatlesPlay(){
 }
 
 function johnLennonFacts(){
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
   var i = 0;
   while(i < facts.length){
     facts[i] + "!!!"; 
